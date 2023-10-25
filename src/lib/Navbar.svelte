a6a4e178c2c72e40bc20e260efc684524fe91c73
Laura Ehlers <laura@ehlers-mail.com>
2023-10-25 20:28:07 +0200

    created project, created navbar

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,16 @@
+<script>
+    const links = ['Home', 'About Me', 'Portfolio', 'Contact'];
+</script>
+
+<main class="p-3">
+    <nav class="navbar bg-primary text-primary-content rounded-3xl ">
+        <div class="flex-1">
+            <p class="btn btn-ghost normal-case text-xl">Laura Ehlers</p>
+        </div>
+        <div class="flex-none">
+            {#each links as link}
+                <li class="menu menu-horizontal px-1">{link}</li>
+            {/each}
+        </div>
+    </nav>
+</main>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Navbar from './../lib/Navbar.svelte';
+    import "../app.css";
+</script>
+
+<Navbar />
